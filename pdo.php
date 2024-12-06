e7b5d1980d0b77dcbc037a8fbddab16d99a2fc00
elmira <elledevelope@gmail.com>
2024-12-06 11:00:48 +0100

    Created 'pdo.php'

---
+++ pdo.php
@@ -0,0 +1,8 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=bibliotheque;charset=utf8', "root", "", [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+]);
+?>
+
