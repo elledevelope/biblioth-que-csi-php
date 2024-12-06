3403f749981ca814e94683c0c2705a6953c1f9fe
elmira <elledevelope@gmail.com>
2024-12-06 10:59:13 +0100

    First commit

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer>
+  <div class="progress">
+    <div class="progress-bar" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+  </div>
+
+  <ul class="ul-footer">
+    <li>
+      <a href="index.php">Accueil
+      </a>
+    </li>
+    <li>
+      <a href="add_livre.php">Ajout Livre</a>
+    </li>
+    <li>
+      <a href="livres.php">Livres</a>
+    </li>
+  </ul>
+
+  <p class="text-secondary">Made by 
+    <a class="text-secondary" href="https://github.com/elledevelope">elledevelope</a><br>
+    2024
+  </p>
+</footer>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php include "header.php" ?>
+<main>
+    <h1>Bienvenue dans la Bibliothèque !</h1>
+    <a href="/add_livre.php">Ajouter un livre</a>
+</main>
+
+<?php include "footer.php"   ?>
+
+
